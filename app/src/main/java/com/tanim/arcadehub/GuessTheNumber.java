6236ff4d2aab46c71559ab40d588a7a018cb4990
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-11-19 13:59:46 +0600

    fixed empty input error in GuessTheNumber

--- app/src/main/java/com/tanim/arcadehub/GuessTheNumber.java
+++ app/src/main/java/com/tanim/arcadehub/GuessTheNumber.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,30 +27,43 @@ public class GuessTheNumber extends AppCompatActivity {
         TextView res = findViewById(R.id.resultText);
 
         check.setOnClickListener(view -> {
-            if (!computerGuessGenerated) {
-                // Generate the initial computerGuess value only once
-                maxLimit = Integer.parseInt(max.getText().toString());
-                computerGuess = (int) ((Math.random() * maxLimit) + 1);
-                computerGuessGenerated = true;
-            }
+            String userInputString = user.getText().toString().trim(); // Trim to remove leading/trailing whitespaces
 
-            int userInput = Integer.parseInt(user.getText().toString());
+            if (!userInputString.isEmpty()) {
+                try {
+                    int userInput = Integer.parseInt(userInputString);
 
-            if (computerGuess == userInput) {
-                check.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
-                res.setText("Correct, you took " + tries + " tries.");
-                computerGuess = (int) ((Math.random() * maxLimit) + 1);
-                tries = 0;
-            } else {
-                check.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
-                if (computerGuess > userInput) {
-                    res.setText("Wrong, Number is higher");
-                } else {
-                    res.setText("Wrong, Number is lower");
+                    if (!computerGuessGenerated) {
+                        // Generate the initial computerGuess value only once
+                        maxLimit = Integer.parseInt(max.getText().toString());
+                        computerGuess = (int) ((Math.random() * maxLimit) + 1);
+                        computerGuessGenerated = true;
+                    }
+
+                    if (computerGuess == userInput) {
+                        check.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                        res.setText("Correct, you took " + tries + " tries.");
+                        computerGuess = (int) ((Math.random() * maxLimit) + 1);
+                        tries = 0;
+                    } else {
+                        check.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                        if (computerGuess > userInput) {
+                            res.setText("Wrong, Number is higher");
+                        } else {
+                            res.setText("Wrong, Number is lower");
+                        }
+                        tries++;
+                    }
+                    user.setText("");
+                } catch (NumberFormatException e) {
+                    // Handle the case where the input is not a valid integer
+                    e.printStackTrace(); // Log the exception for debugging
                 }
-                tries++;
+            } else {
+                Toast.makeText(GuessTheNumber.this, "Empty Input", Toast.LENGTH_SHORT).show();
             }
-            user.setText("");
         });
+
+
     }
 }
